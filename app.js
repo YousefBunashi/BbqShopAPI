7162a290586e2fc5289d2fea38a12953a777cb0c
YousefBunashi <bunashi88@hotmail.com>
2020-11-22 19:26:55 +0300

    add

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 const express = require("express");
 const cors = require("cors");
-const bbqs = require("./bbqs");
+let bbqs = require("./bbqs");
+const bodyParser = require("body-parser");
+const slugify = require("slugify");
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
-app.get("/bbqs", (req, response) => {
+app.get("/bbqs", (req, res) => {
   res.json(bbqs);
+});
+
+app.delete("/bbqs/:bbqId", (req, res) => {
+  const { bbqId } = req.params;
+  const foundBbq = bbqs.find((bbq) => bbq.id === +bbqId);
+
+  if (foundBbq) {
+    bbqs = bbqs.filter((bbq) => bbq.id !== +bbqId);
+    res.status(204).end();
+  } else {
+    res.status(404).json({ message: "Bbq not found" });
+  }
+});
+app.post("/bbqs", (req, res) => {
+  const id = bbqs[bbqs.length - 1].id + 1;
+  const slug = slugify(req.body.name, { lower: true });
+  const newBbq = { id, slug, ...req.body };
+  bbqs.push(newBbq);
+  res.status(201).json(newBbq);
 });
 
 app.listen(8000, () => {
