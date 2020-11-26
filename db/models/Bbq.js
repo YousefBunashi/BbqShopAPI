45ce029f7efd912549e5f382d6db6e3cdeca69b4
YousefBunashi <bunashi88@hotmail.com>
2020-11-26 10:32:39 +0300

    job is done

--- db/models/Bbq.js
+++ db/models/Bbq.js
@@ -1,26 +1,35 @@
 const SequelizeSlugify = require("sequelize-slugify");
+
 module.exports = (sequelize, DataTypes) => {
   const Bbq = sequelize.define("Bbq", {
     name: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
-
     slug: {
       type: DataTypes.STRING,
       unique: true,
     },
     description: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     price: {
       type: DataTypes.INTEGER,
-    },
-    image: {
-      type: DataTypes.STRING,
+      defaultValue: 5,
+      validate: {
+        min: 1,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
   });
+
   SequelizeSlugify.slugifyModel(Bbq, {
     source: ["name"],
   });
+
   return Bbq;
 };
